1788d69ce3ed408b66e92140d00cdc32a38008f6
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-06-18 17:58:38 +0200

    Function documentation improvements

--- SharpEval.Core/IO/FunctionDocumentationProvider.cs
+++ SharpEval.Core/IO/FunctionDocumentationProvider.cs
@@ -1,13 +1,17 @@
 ﻿using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 
 using SharpEval.Core.Internals;
+
+using UnitsNet;
 
 namespace SharpEval.Core.IO
 {
     /// <summary>
     /// Provides documentation for globaly registered functions
     /// </summary>
-    public sealed class FunctionDocumentationProvider
+    public sealed partial class FunctionDocumentationProvider
     {
         /// <summary>
         /// Documentations
@@ -56,15 +60,66 @@ namespace SharpEval.Core.IO
 
                 var doc = member.GetDocumentation();
 
+                if (string.IsNullOrEmpty(doc)) continue;
+
+                string cleanDoc = Clean(doc);
+
                 if (Documentation.TryGetValue(key, out List<string>? value))
                 {
-                    value.Add(doc);
+                    value.Add(cleanDoc);
                 }
                 else
                 {
-                    Documentation.Add(key, new List<string> { doc });
+                    Documentation.Add(key, new List<string> { cleanDoc });
                 }
             }
+        }
+
+        [GeneratedRegex("<param name=\"(.+)\">(.+)<\\/param>")]
+        private static partial Regex ParamRegex();
+
+        private static string Clean(string doc)
+        {
+            const string elementSummary = "<summary>";
+            const string elementSummaryEnd = "</summary>";
+            const string elementReturns = "<returns>";
+
+            StringBuilder final = new StringBuilder();
+            int paramCounter = 0;
+            using (var reader = new StringReader(doc))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var trimmed = line.Trim();
+                    if (trimmed == elementSummary)
+                    {
+                        final.AppendLine("Description:");
+                    }
+                    else if (trimmed == elementSummaryEnd)
+                    {
+                        final.AppendLine();
+                    }
+                    else if (trimmed.StartsWith(elementReturns))
+                    {
+                        continue;
+                    }
+                    else if (ParamRegex().IsMatch(line))
+                    {
+                        if (paramCounter == 0)
+                        {
+                            final.AppendLine("Parameters:");
+                        }
+                        final.AppendLine(ParamRegex().Replace(line, "$1: $2"));
+                        ++paramCounter;
+                    }
+                    else
+                    {
+                        final.AppendLine(line);
+                    }
+                }
+            }
+            return final.ToString();
         }
     }
 }
