e183f7bd4a68a2fdfe09ad3d29ae7d7e68bf3b08
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-07-27 10:32:19 +0200

    Base work on Hashing functions

---
+++ SharpEval.Core/Maths/Hashing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEval.Core.Maths;
+internal static class Hashing
+{
+    private static byte[] GetData(string @string)
+    {
+        return Encoding.UTF8.GetBytes(@string);
+    }
+
+}
