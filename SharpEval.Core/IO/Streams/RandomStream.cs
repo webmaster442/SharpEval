82e6fdb4cdae8e19a28fc7968024f8f4702a8c01
webmaster442 <ruzsinszki.gabor@gmail.com>
2023-08-19 21:07:47 +0200

    Zero and Random streams

---
+++ SharpEval.Core/IO/Streams/RandomStream.cs
@@ -0,0 +1,48 @@
+﻿namespace SharpEval.Core.IO.Streams;
+internal sealed class RandomStream : Stream
+{
+    private long _position;
+
+    public override bool CanRead => true;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => false;
+
+    public override long Length => long.MaxValue;
+
+    public override long Position 
+    {
+        get => _position;
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        byte[] generated = new byte[count - offset];
+        Random.Shared.NextBytes(generated);
+        Array.Copy(generated, 0, buffer, 0, generated.Length);
+        _position += generated.Length;
+        return generated.Length;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+}
---
+++ SharpEval.Core/IO/Streams/ZeroStream.cs
@@ -0,0 +1,48 @@
+﻿namespace SharpEval.Core.IO.Streams;
+
+internal class ZeroStream : Stream
+{
+    private long _position;
+
+    public override bool CanRead => true;
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => false;
+
+    public override long Length => long.MaxValue;
+
+    public override long Position
+    {
+        get => _position;
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        Array.Fill<byte>(buffer, 0, offset, count);
+        int generated = count - offset;
+        _position += generated;
+        return generated;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+}
